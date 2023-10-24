2fe6c5c533a50c8a77a31386c7774b927ab99d8d
saul sl <ssl-work@proton.me>
2023-10-24 16:21:40 -0400

    Improved layout using css
    - custom css was extended to simplify writing slides
    - custom js defines colors for backgrounds based on theme
    - local dist folder with reveal.js files was deleted, now using a cdn

--- README.md
+++ README.md
@@ -1,9 +1,10 @@
 # Reveal.js presentation written in emacs org-mode + org-reveal
-
-
 ## Description
 
-[Reveal.js](https://revealjs.com/) is a tool for creating HTML presentations and [org-reveal](https://github.com/yjwen/org-reveal/) is an emacs package that converts org files into reveal.js-compatible HTML format. Although org-reveal greatly simplifies the process of creating a presentation, some tweaking is necessary. The code shows such modifications used for my PhD. thesis dissertation.
+[Reveal.js](https://revealjs.com/) is a tool for creating HTML presentations and [org-reveal](https://github.com/yjwen/org-reveal/) is an emacs package that converts org files into reveal.js-compatible HTML format. Although org-reveal greatly simplifies the process of creating a presentation, some modifications are necessary. The code shows such modifications using as example part of the presentation used for my PhD. thesis dissertation.
+
+# Links
 
 -   [Live version](https://ssl-bio.github.io/Reveal.js-demo/#/slide-org4ffdd88).
--   [Description](https://ssl-blog.netlify.app/posts/reveal-js-presentations/)
+-   [Description (Original)](https://ssl-blog.netlify.app/posts/reveal-js-presentations/)
+-   [Description (Modifications)](https://ssl-blog.netlify.app/posts/reveal-js-demo2/)
--- UT/js/icustom.js
+++ UT/js/icustom.js
@@ -1,34 +1,88 @@
+// Get slide's dimensions
+slideHeight=getComputedStyle(document.body).getPropertyValue('--slide-height');
+document.documentElement.style.setProperty('--slide-height',slideHeight);
+
 // Define background colors and initial theme
-bg=getComputedStyle(document.body).getPropertyValue('--r-link-color');
-bg=bgLen(bg);
-let bgon=bg+"b2";
-document.documentElement.style.setProperty('--r-bgon',bgon);
-    
+var [bgMsg, bgColorA, bgColorB] = getColor();
+
+// Set the color value
+document.documentElement.style.setProperty('--clr-bg-message',bgMsg);
+document.documentElement.style.setProperty('--clr-bg-A',bgColorA);
+document.documentElement.style.setProperty('--clr-bg-B',bgColorB);
+
+// rgba to hex (from https://stackoverflow.com/a/73401564/12414497)
+function rgba2hexa(rgba, forceRemoveAlpha = false) {
+  return "#" + rgba.replace(/^rgba?\(|\s+|\)$/g, '')
+    .split(',')
+    .filter((string, index) => !forceRemoveAlpha || index !== 3)
+    .map(string => parseFloat(string))
+    .map((number, index) => index === 3 ? Math.round(number * 255) : number)
+    .map(number => number.toString(16))
+    .map(string => string.length === 1 ? "0" + string : string)
+    .join("")
+}
+
 // Return the value of the link color
 function getColor(){
-    bg=getComputedStyle(document.body).getPropertyValue('--r-link-color');
-    bg=bgLen(bg);
-    let bgon=bg+"b2";
-    return bgon;
+    clrMain=getComputedStyle(document.body).getPropertyValue('--r-main-color');
+    clrBg=getComputedStyle(document.body).getPropertyValue('--r-background-color');
+    clrLink=getComputedStyle(document.body).getPropertyValue('--r-link-color');
+    
+
+    if (clrMain.startsWith('#')){
+	clrMain=clrNorm(clrMain);
+    } else {
+	clrMain=rgba2hexa(clrMain);
+	clrMain=clrNorm(clrMain);
+    }
+
+    if (clrBg.startsWith('#')){
+	clrBg=clrNorm(clrBg);
+    } else {
+	clrBg=rgba2hexa(clrBg);
+	clrBg=clrNorm(clrBg);
+    }
+
+    if (clrLink.startsWith('#')){
+	clrLink=clrNorm(clrLink);
+    } else {
+	clrLink=rgba2hexa(clrLink);
+	clrLink=clrNorm(clrLink);
+    }
+    
+    let bgMsg=clrBg+"bf"; // bf = 75% opacity
+    let bgColorA=clrLink+"26"; // 26 = 15% opacity
+    let bgColorB=clrMain+"40"; // 40 = 25% opacity
+
+    return [bgMsg, bgColorA, bgColorB];
 }
 
 // Return a six character color, useful if a 3 letter is supplied
-function bgLen(bg){
-    col=bg.replace("#","");
+function clrNorm(clr){
+    col=clr.replace("#","");
     if (col.length == 3){
-	bg="#"+col[0].repeat(2)+col[1].repeat(2)+col[2].repeat(2);
+	clr="#"+col[0].repeat(2)+col[1].repeat(2)+col[2].repeat(2);
+    } else if ((col.length == 8)) {
+	clr="#"+col.slice(0,5)
     }
-    return bg;
+    
+    return clr;
 }
 
 
 // Function to update background colors of div and button elements
 function updateBGcol(){
-    currColor = getColor();
-    if ( currColor != bgon ){
+    var [bgMsgCurr, bgColorACurr, bgColorBCurr] = getColor();
+    if ( bgMsgCurr != bgMsg ||
+	 bgColorACurr != bgColorA ||
+	 bgColorB != bgColorBCurr ){
 	console.log("updating color");
-	bgon=currColor;
-	document.documentElement.style.setProperty('--r-bgon',bgon);
+	bgMsg=bgMsgCurr;
+	bgColorA=bgColorACurr;
+	bgColorB=bgColorBCurr;
+	document.documentElement.style.setProperty('--clr-bg-message',bgMsg);
+	document.documentElement.style.setProperty('--clr-bg-A',bgColorA);
+	document.documentElement.style.setProperty('--clr-bg-B',bgColorB);
     }  
 }
 
@@ -41,7 +95,15 @@ function ilegend(){
 	id=islide.id;
 	isec=document.getElementById(id);
 	idiv=isec.children[0];
-	idiv.classList.toggle('out');
+	if (isec.style.backgroundColor === 'transparent') {
+	    // Set the background color
+	    isec.style.backgroundColor = bgMsg;
+	    idiv.classList.toggle('out');
+	} else {
+	    // Remove the background color
+	    isec.style.backgroundColor = 'transparent';
+	    idiv.classList.toggle('out');
+	}
 	updateBGcol();
     }
 }
